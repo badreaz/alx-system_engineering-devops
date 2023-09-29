d3a83a9d6b58c4b8415a073e9dd1db9304543da7
badreaz <baz.23.2019@gmail.com>
2023-09-29 21:52:36 +0400

    create zombies

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - infinite loop
+ * Return: 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+		sleep(1);
+	return (0);
+}
+
+/**
+ * main - create 5 zombies
+ * Return: 0.
+ */
+int main(void)
+{
+	int i;
+	pid_t pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+	infinite_while();
+	return (0);
+}
