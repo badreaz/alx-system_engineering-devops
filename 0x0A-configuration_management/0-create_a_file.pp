67de1e9ebf6fa4455b75e6457f9e0fc460b2353c
badreaz <baz.23.2019@gmail.com>
2023-11-24 20:24:13 +0400

    edit ensure

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file in '/tmp'
 file { '/tmp/school':
-  ensure  => 'file',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0774',
-  content => 'I love Puppet',
+    ensure  => 'present',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0774',
+    content => 'I love Puppet',
 }
