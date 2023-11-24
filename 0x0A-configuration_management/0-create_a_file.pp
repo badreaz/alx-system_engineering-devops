6f4ec0b07647565a9f2baf8271729c7ceb9c4d55
badreaz <baz.23.2019@gmail.com>
2023-11-24 20:57:10 +0400

    fix file permission

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,6 +3,6 @@ file { '/tmp/school':
     ensure  => 'present',
     owner   => 'www-data',
     group   => 'www-data',
-    mode    => '0774',
+    mode    => '0744',
     content => 'I love Puppet',
 }
