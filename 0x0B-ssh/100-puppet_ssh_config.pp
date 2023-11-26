0885b27577d970610dd58cfd9b17ad0232c7c5d0
badreaz <baz.23.2019@gmail.com>
2023-11-26 20:22:20 +0400

    fix spelling

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '	PasseordAuthentification no',
+  line   => '	PasswordAuthentication no',
 }
 
-file_line { 'Declare identify file':
+file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '   IdentifyFile ~/.ssh/school',
+  line   => '   IdentityFile ~/.ssh/school',
 }
